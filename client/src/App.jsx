1271a72155f4a5ad955abd9ec696d45499175d6f
gargabhishek0987 <gargabhishek742@gmial.com>
2025-06-22 00:57:29 +0530

    adding homepage section  with some text

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home';
 
 
 
@@ -13,6 +14,12 @@ const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div>
       {!isOwnerPath && < Navbar />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+        </Routes>
+
+      </div>
     </div>
   )
 }
