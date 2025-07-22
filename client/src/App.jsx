8d1dbdb57d3c66df814629f41c60045b13217729
gargabhishek0987 <gargabhishek742@gmial.com>
2025-07-23 00:21:27 +0530

    added the Room details Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import AllRooms from './pages/AllRooms'
+import RoomDetails from './pages/RoomDetails';
 
 
 const App = () => {
@@ -18,8 +19,8 @@ const isOwnerPath = useLocation().pathname.includes("owner");
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/rooms' element={<AllRooms/>} />
-
+          <Route path='/rooms' element={<AllRooms />} />
+          <Route path='/rooms/:id' element={<RoomDetails/>} />
         </Routes>
 
 
